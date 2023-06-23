6bfe97cc8b9d00492b42beb96d65520ab1c0808c
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2023-06-23 07:17:40 +0000

    [Finish]課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,26 +4,60 @@ class BooksController < ApplicationController
   end
 
   def create
-    
-    book = Book.new(book_params)
-    
-    book.save
-    
-    redirect_to '/top'
+
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+
   end
 
 
   def index
+     @books = Book.all
+     @book = Book.new
   end
+
 
   def show
+     @books = Book.all
+     @book = Book.find(params[:id])
   end
+  
 
   def edit
+    @books = Book.all
+    @book = Book.find(params[:id])
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :edit
+  end
+  
+end
+
+def destroy
+    book = Book.find(params[:id]) 
+    book.destroy  
+    redirect_to '/books'  
+    
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
